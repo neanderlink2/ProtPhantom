d1f5dc654b603a05a29fedc1c1e455e3e67c3cfa
Neander Wendel <neanderlink123@gmail.com>
2019-01-20 08:12:53 -0200

    Primeiro commit

---
+++ Assets/Scripts/Movement/Interfaces/IMovimentavel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMovimentavel
+{
+    float Velocidade { get; set; }
+    float ForcaPulo { get; set; }
+    void Mover(Vector3 direcao);
+    void Pular();
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MovementBase))]
+public class PlayerController : MonoBehaviour
+{
+    private MovementBase m_move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_move = GetComponent<MovementBase>();
+    }
+
+    public void Update()
+    {
+        var direcao = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));        
+        m_move.Mover(direcao);
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            m_move.Pular();
+        }
+
+    }
+}
